6de050079e1245ab7e053c555990d0a5276cbbd5
zenefono <zenefono@gmail.com>
2022-01-26 07:59:33 +0100

    clean from testing

--- startMuenu.sh
+++ startMuenu.sh
@@ -6,15 +6,15 @@
 # myFolder="$(pwd)/mieifile"
 myFolder="$(pwd)/voci_menu"
 myArray=( $(ls -1p $myFolder ) )
-#  myArray=( $(ls -1p $myFolder | sed "s/^\(.*\)/'\1'/" ) )
+# myArray=( $(ls -1p $myFolder | sed 's/^\(.*\)/"\1"/' ) ) # per mettere le virgolette
 
-read -p "echo myArray..."
-echo "${myArray[*]}"
-echo "array of ${#myArray[@]} elements"
-read -p "press enter..."
+# read -p "echo myArray..."
+# echo "${myArray[*]}"
+# echo "array of ${#myArray[@]} elements"
+# read -p "press enter..."
 # exit 0
 
-myArray2=("TERMINA " "selezionare per uscire") 
+myArray2=("TERMINA " "# Selezionare per uscire" "" "") 
 for i in "${myArray[@]}"; do
   myDesc="tail -n +3 $myFolder/$i"
 #  myDesc="tail -n+3 $myFolder/$i|head -1"
@@ -22,20 +22,20 @@ for i in "${myArray[@]}"; do
   # myDesc="sed -n '3p' < $myFolder/$i"
   #myDesc="sed \"1q;d\"  $myFolder/$i"
 
-  echo "$myFolder"
-  echo "$myFolder/$i"
-# echo "stampo myDesc"
-  echo $myDesc
+#   echo "$myFolder"
+#   echo "$myFolder/$i"
+# # echo "stampo myDesc"
+#   echo $myDesc
   myDesc1="$($myDesc)"
-  echo $myDesc1
-  read -p "premi ENTER per continuare... "
+#   echo $myDesc1
+#   read -p "premi ENTER per continuare... "
   myArray2+=("$i" "$myDesc1")
 done
 
-read -p "echo myArray2..."
-echo "${myArray2[*]}"
-echo "array of ${#myArray2[@]} elements"
-read -p "press enter..."
+# read -p "echo myArray2..."
+# echo "${myArray2[*]}"
+# echo "array of ${#myArray2[@]} elements"
+# read -p "press enter..."
 
 while [ "$result" != "TERMINA " ]; do
   result=$( whiptail --title "xTITLEx" --menu "choose: " 16 78 10 "${myArray2[@]}" 3>&2 2>&1 1>&3 )
